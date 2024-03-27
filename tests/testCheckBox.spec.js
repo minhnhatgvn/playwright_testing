5c152f84b073d9691ec4df55018e0ffc02cde6ea
Nhat Thai <thaiminhnhat@gmail.com>
2024-03-28 00:16:41 +0700

    add test for Check box page

--- tests/testCheckBox.spec.js
+++ tests/testCheckBox.spec.js
@@ -1,4 +1,9 @@
 // @ts-check
+/* The test will verify data table on the url https://the-internet.herokuapp.com/checkboxes:
+- The url is  contain /checkboxes
+- The elements is displayed correctly 2 checkboxes
+- Able to check/uncheck the checkboxes
+*/
 const { test, expect } = require("@playwright/test");
 const { CheckBoxPage } = require("./../pages/CheckBoxPage");
 const { HomePage } = require("./../pages/HomePage");
@@ -8,5 +13,5 @@ test("Checkbox page test", async ({ page }) => {
   const Home = new HomePage(page);
   await Home.goToHomePage();
   await Home.goToCheckBoxLink();
-  await CheckBox.checkFirstCheckBox();
+  await CheckBox.verifyCheckboxPage();
 });
