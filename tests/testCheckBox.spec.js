82c9c88bc9f8912a3757c78b628074a8e279cb90
Nhat Thai <thaiminhnhat@gmail.com>
2024-04-14 21:59:34 +0700

    Update testCheckBox.spec.js

--- tests/testCheckBox.spec.js
+++ tests/testCheckBox.spec.js
@@ -1,4 +1,4 @@
-// @ts-check
+
 /* The test will verify data table on the url https://the-internet.herokuapp.com/checkboxes:
 - The url is  contain /checkboxes
 - The elements is displayed correctly 2 checkboxes
